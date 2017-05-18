00028e2c17209a48b14989c10bbac8ce6bd62e18
Maryna Havrylova <Maryna@PHIDIAS.NL>
2017-05-18 17:49:04 +0300

    updated repo

--- evoplay/src/js/main.js
+++
@@ -1,4 +0,0 @@
-
-//= ../../bower_components/jquery/dist/jquery.min.js
-//= ../../bower_components/jquery-slimscroll/jquery.slimscroll.min.js
-//= partials/script.js
--- godate/src/js/main.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-
-//= partials/script.js
