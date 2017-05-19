395fb95a097dea300f91100c017d10ab915b792a
Maryna Havrylova <Maryna@PHIDIAS.NL>
2017-05-19 13:09:45 +0300

    new portfolio

---
+++ portfolio/src/js/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+
+/*
+ * Third party
+ */
+//= ../../bower_components/jquery/dist/jquery.min.js
+//= ../../bower_components/bootstrap/dist/js/bootstrap.min.js
+
+/*
+ * Custom
+ */
+//= partials/script.js
